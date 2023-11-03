ee751d9c67ffb3b184016e11ecca462f875c43dc
Henry Ifebunandu <henryifebunandu@gmail.com>
2023-11-03 14:07:37 +0100

    Initial Commit

---
+++ SwiftUiSlots/SwiftUiSlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiSlotsApp.swift
+//  SwiftUiSlots
+//
+//  Created by Henry Ifebunandu on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiSlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
